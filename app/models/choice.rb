a07dfb5b41d38355f0846e6d672396a21ab0dde2
shreshtt <shreshttbhatt90@gmail.com>
2013-05-12 14:05:03 +0530

    t

---
+++ app/models/choice.rb
@@ -0,0 +1,4 @@
+class Choice < ActiveRecord::Base
+  attr_accessible :text, :correct, :question_id
+  belongs_to :question
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exam::Application.config.secret_token = 'b927c778d75f5a8d7db46db5cbede7523b733c5df59731cb9ec803c5584710ef7282d8921f673ba032e746277d8405f8c0afc10bf889e52798bd71756fe17367'
